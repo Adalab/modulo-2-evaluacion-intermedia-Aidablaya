67052eeba3a1f0f676b22676b0baaafb72d96caf
Aidablaya <you@example.com>
2023-05-31 10:59:09 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const bet =document.querySelector('.js_advice');
 const advice= document.querySelector('.js_advice');
 const balance= document.querySelector('.js_balance');
 const btn= document.querySelector('.js_btn');
-
+/*
 //EVENTOS click
 function handleClickBtn(ev) {
     //ev.preventDefault();
@@ -34,13 +34,37 @@ function handleClickBtn(ev) {
         advice.textContent = "Vamos a jugar";
     }
 
-}
+}*/
 console.log (handleClickBtn());
 
 
 btn.addEventListener("click", handleClickBtn);
 
+//funcion saldo
+let userBalance = 50;
+    balance.textContent = `Saldo ${userBalance}`
 
+function handleClickBtn(ev) {
+    //ev.preventDefault();
+    const selectOption = parseInt(select.value);
+    console.log (selectOption);
+    const randomNumber = getRandomNumber(6);
+    console.log (randomNumber);
+    
+    
+    //condición
+    if (selectOption === randomNumber ) {
+        advice.textContent = "Has ganado el doble de lo apostado";
+        const win = userBalance * 2
+        win += userBalance;
+        
+    }else if (selectOption !== randomNumber) {        
+        advice.textContent = "Has perdido lo apostado";
+    }else {
+        advice.textContent = "Vamos a jugar";
+    }
+
+}
 //FUNCIONES
 //Función generadora numero aleatorio 1/6
 
